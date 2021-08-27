652552a5352f44e9e68fac03ed48b60926748e9a
Serhii Romanov <serhii_romanov@epam.com>
2021-08-27 13:25:31 +0300

    [EDP-DDM-8073] Kafka ssl

--- src/main/java/com/epam/digital/data/platform/starter/actuator/config/ActuatorConfig.java
+++ src/main/java/com/epam/digital/data/platform/starter/actuator/config/ActuatorConfig.java
@@ -1,8 +1,10 @@
 package com.epam.digital.data.platform.starter.actuator.config;
 
 import com.epam.digital.data.platform.starter.actuator.readinessprobe.UrlAvailabilityChecker;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
 import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -41,6 +43,28 @@ public class ActuatorConfig {
     props.put(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
         pf.getConfigurationProperties().get(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
+
+    props.putAll(sslProps(pf));
+
     return AdminClient.create(props);
+  }
+
+  private <O> Map<String, Object> sslProps(ProducerFactory<String, O> pf) {
+    Map<String, Object> props = new HashMap<>();
+
+    putIfPresent(pf, props, CommonClientConfigs.SECURITY_PROTOCOL_CONFIG);
+    putIfPresent(pf, props, SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG);
+    putIfPresent(pf, props, SslConfigs.SSL_KEYSTORE_TYPE_CONFIG);
+    putIfPresent(pf, props, "ssl.truststore.certificates");
+    putIfPresent(pf, props, "ssl.keystore.certificate.chain");
+    putIfPresent(pf, props, "ssl.keystore.key");
+
+    return props;
+  }
+
+  private <O> void putIfPresent(ProducerFactory<String,O> pf, Map<String, Object> props, String prop) {
+    if (pf.getConfigurationProperties().get(prop) != null) {
+      props.put(prop, pf.getConfigurationProperties().get(prop));
+    }
   }
 }
